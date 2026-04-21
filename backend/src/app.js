fdc137b8a2a73acbb7627365e6898ced985a986b
R-JDiaz <diazrj54@gmail.com>
2026-04-21 19:10:55 +0800

    fix: correct route paths for products, users, and carts

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,18 @@ import express from 'express';
 import userRoutes from './features/user/user.routes.js'
 import productRoutes from "./features/products/product.routes.js";
 import cartRoutes from "./features/cart/cart.routes.js";
+
+import { globalErrorHandler } from './common/utilities/handler.js';
 const app = express();
 
 
 app.use(express.json());
 
 
-app.use("/products", productRoutes);
-app.use('/users', userRoutes);
-app.use('/carts', cartRoutes);
+app.use('/product', productRoutes);
+app.use('/user', userRoutes);
+app.use('/cart', cartRoutes);
 
+app.use(globalErrorHandler);
 
 export default app;
