d896d4d31c17220b152930d996351521d2c57c07
R-JDiaz <diazrj54@gmail.com>
2026-04-22 19:57:13 +0800

    feat: add payment routes to the application

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import userRoutes from './features/user/user.routes.js'
 import productRoutes from "./features/products/product.routes.js";
 import cartRoutes from "./features/cart/cart.routes.js";
 import orderRoutes from "./features/order/order.routes.js";
+import paymentRoutes from "./features/payment/payment.routes.js";
 
 import { globalErrorHandler } from './common/utilities/handler.js';
 const app = express();
@@ -16,6 +17,7 @@ app.use('/product', productRoutes);
 app.use('/user', userRoutes);
 app.use('/cart', cartRoutes);
 app.use('/orders', orderRoutes);
+app.use('/payment', paymentRoutes);
 
 app.use(globalErrorHandler);
 
