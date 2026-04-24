1fe357fe286ee088f7f451c75b638dd5a523f4e4
R-JDiaz <diazrj54@gmail.com>
2026-04-24 21:29:59 +0800

    impl updates

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { AdminDashboard } from '@features/admin-dashboard/admin-dashboard';
 import { Checkout } from '@features/user-dashboard/checkout/checkout';
 import { Landing } from '@features/landing/landing';
 import { Login } from '@features/auth/login/login';
-import { Signup } from '@features/auth/signup/signup';
 import { Orders } from '@features/user-dashboard/orders/orders';
 import { Profile } from '@features/user-dashboard/profile/profile';
 import { UserDashboard } from '@features/user-dashboard/user-dashboard';
@@ -11,7 +10,6 @@ import { UserDashboard } from '@features/user-dashboard/user-dashboard';
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'login', component: Login },
-  { path: 'signup', component: Signup },
   { path: 'user-dashboard', component: UserDashboard },
   { path: 'checkout', component: Checkout },
   { path: 'orders', component: Orders },
