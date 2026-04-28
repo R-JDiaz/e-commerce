fad91aada0f370f192b0039b6279ab2f61b3eff8
R-JDiaz <diazrj54@gmail.com>
2026-04-28 19:38:20 +0800

    feat: imp OrderManager for it to load its own datas

--- frontend/src/app/features/admin-dashboard/orders/orders.ts
+++ frontend/src/app/features/admin-dashboard/orders/orders.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Order } from '@common/services/managers/order/order';
+import { Order, OrderManager } from '@common/services/managers/order/order';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-admin-orders',
@@ -9,7 +10,18 @@ import { Order } from '@common/services/managers/order/order';
   templateUrl: './orders.html',
   styleUrl: './orders.scss',
 })
-export class AdminOrdersComponent {
-  @Input() orders: Order[] = [];
+export class AdminOrdersComponent implements OnInit{
+  orders$!: Observable<Order[]>;
   @Output() statusChange = new EventEmitter<{ id: string; status: Order['status'] }>();
+
+  constructor (
+    public manager: OrderManager
+  ) {}
+
+  ngOnInit(): void {
+    this.orders$ = this.manager.getAllOrders();
+    this.orders$.pipe(
+      tap(order => console.log(order))
+    ).subscribe();
+  }
 }
