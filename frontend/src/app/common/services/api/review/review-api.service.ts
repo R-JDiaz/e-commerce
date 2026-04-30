dbe8da4b6980e9c6e649c6db3326e20aa1617e47
R-JDiaz <diazrj54@gmail.com>
2026-05-01 04:50:37 +0800

    feat: api service for review

---
+++ frontend/src/app/common/services/api/review/review-api.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../../environments/environment';
+
+import {
+  CreateOrderReviewDTO,
+  OrderReviewDTO
+} from '@common/dtos/review.dto';
+
+export type CreateReviewRequest = CreateOrderReviewDTO;
+export type Review = OrderReviewDTO;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewApiService {
+
+  private readonly baseUrl = `${environment.apiBaseUrl}/reviews`;
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Create review for an order
+   */
+  createReview(payload: CreateReviewRequest): Observable<Review> {
+    return this.http.post<Review>(this.baseUrl, payload);
+  }
+
+  /**
+   * Get all reviews of logged-in user
+   */
+  getMyReviews(): Observable<Review[]> {
+    return this.http.get<Review[]>(this.baseUrl);
+  }
+
+  /**
+   * Get review by order ID
+   */
+  getReviewByOrderId(orderId: number): Observable<Review> {
+    return this.http.get<Review>(`${this.baseUrl}/order/${orderId}`);
+  }
+
+  /**
+   * Delete review by ID
+   */
+  deleteReview(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
