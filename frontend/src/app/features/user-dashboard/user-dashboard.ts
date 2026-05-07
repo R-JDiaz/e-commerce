2572740e35ec0a54bf3c8cb7db98f25daa141a7d
R-JDiaz <diazrj54@gmail.com>
2026-05-07 14:17:00 +0800

    feat: user-dsahboard comp

--- frontend/src/app/features/user-dashboard/user-dashboard.ts
+++ frontend/src/app/features/user-dashboard/user-dashboard.ts
@@ -9,13 +9,19 @@ import { NavigationComponent } from '@common/components/navigation/navigation';
 import { Products } from './products/products';
 import { OrderManager } from '@common/services/managers/order/order';
 import { NotificationManager } from '@common/services/managers/notification/notification.manager';
+import { SupportManager } from '@common/services/managers/support/support';
+import { ProductManager } from '@common/services/managers/product/product';
+import { ProductDisplayComponent } from './product-display/product-display';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-dashboard',
   standalone: true,
   imports: [
      Products, 
-     NavigationComponent
+     NavigationComponent,
+     ProductDisplayComponent,
+     CommonModule
     ],
   templateUrl: './user-dashboard.html',
   styleUrl: './user-dashboard.scss',
@@ -26,7 +32,9 @@ export class UserDashboard implements OnInit {
     private notifManager: NotificationManager,
     private authService: AuthManager,
     private router: Router,
-    private OrderManager: OrderManager
+    private OrderManager: OrderManager,
+    private supportManager: SupportManager,
+    public productManager: ProductManager
   ) {}
   
   ngOnInit(): void {
@@ -36,9 +44,14 @@ export class UserDashboard implements OnInit {
     this.CartManager.addToCart(product);
   }
 
+  closeProductDisplay() {
+    this.productManager.deselectProduct();
+  }
+
   logout() {
     this.OrderManager.clearState();
     this.notifManager.clearNotifications();
+    this.supportManager.resetSupportState();
     this.CartManager.clearCart();
     this.authService.logout();
     this.router.navigate(['/login']);
