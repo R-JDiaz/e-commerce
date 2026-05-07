47c3db90ba211d4eaf11ab8ef1248f3a8368f669
R-JDiaz <diazrj54@gmail.com>
2026-05-07 12:50:30 +0800

    feat: update users seed

--- backend/src/core/database/seeds/001_users_seed.sql
+++ backend/src/core/database/seeds/001_users_seed.sql
@@ -5,7 +5,7 @@ VALUES
 ('admin2@example.com', '$2b$10$hash_admin_2', 'Main', 'Admin', '09170000002', 'admin'),
 
 -- Customers
-('john.doe@example.com', '$2b$10$hash_user_1', 'John', 'Doe', '09170000003', 'customer'),
+('john.doe@example.com', '$2b$10$hash_user_1', 'Jay', 'Entiliso', '09170000003', 'customer'),
 ('jane.smith@example.com', '$2b$10$hash_user_2', 'Jane', 'Smith', '09170000004', 'customer'),
 ('pedro.reyes@example.com', '$2b$10$hash_user_3', 'Pedro', 'Reyes', '09170000005', 'customer'),
 ('ana.garcia@example.com', '$2b$10$hash_user_4', 'Ana', 'Garcia', '09170000006', 'customer'),
